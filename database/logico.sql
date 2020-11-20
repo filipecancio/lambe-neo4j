eb5639c6c477f41c57aa6c5c46619c610a951bb6
Cancio <filipe.cancio@outlook.com>
2020-11-20 14:55:18 -0300

    update create

--- database/logico.sql
+++ database/logico.sql
@@ -1,64 +1,65 @@
-/* logico: */
 
-CREATE TABLE User (
-    password VARCHAR,
-    id SMALLINT PRIMARY KEY,
-    name VARCHAR,
-    lastname VARCHAR,
-    cpf VARCHAR,
-    birthday DATE,
-    description VARCHAR,
-    avatar VARCHAR
+drop database if exists Lambe;
+create database Lambe;
+use Lambe;
+
+drop table if exists Contact;
+create table Contact (
+    id smallint primary key,
+    email varchar(50),
+    instagram varchar(50),
+    facebook varchar(50),
+    phone varchar(50)
 );
 
-CREATE TABLE Client (
-    nickname VARCHAR,
-    id SMALLINT PRIMARY KEY,
-    name VARCHAR,
-    lastname VARCHAR,
-    cpf VARCHAR,
-    birthday DATE,
-    description VARCHAR,
-    avatar VARCHAR
+drop table if exists User;
+create table User (
+    password varchar(50),
+    id smallint primary key,
+    name varchar(50),
+    lastname varchar(50),
+    cpf varchar(50),
+    birthday date,
+    description varchar(50),
+    avatar varchar(500),
+    contact_id smallint,
+    foreign key (Contact_id) references Contact (id) on delete cascade
 );
 
-CREATE TABLE Product (
-    id SMALLINT PRIMARY KEY,
-    title VARCHAR,
-    subtitle VARCHAR,
-    description VARCHAR,
-    Value FLOAT
+drop table if exists Client;
+create table Client (
+    nickname varchar(50),
+    id smallint primary key,
+    name varchar(50),
+    lastname varchar(50),
+    cpf varchar(50),
+    birthday date,
+    description varchar(500),
+    avatar varchar(500),
+    contact_id smallint,
+    foreign key (contact_id) references Contact (id) on delete cascade
 );
 
-CREATE TABLE Contact (
-    email VARCHAR,
-    instagram VARCHAR,
-    facebook VARCHAR,
-    phone VARCHAR
+drop table if exists Product;
+create table Product (
+    id smallint primary key,
+    title varchar(50),
+    subtitle varchar(50),
+    description varchar(500),
+    value float
 );
 
-CREATE TABLE Job (
-    fk_User_id SMALLINT,
-    fk_Client_id SMALLINT,
-    fk_Product_id SMALLINT,
-    title VARCHAR,
-    subtitle VARCHAR,
-    sheduling DATE,
-    delivery DATE,
-    description VARCHAR
+drop table if exists Job;
+create table Job (
+    user_id smallint,
+    client_id smallint,
+    product_id smallint,
+    title varchar(50),
+    subtitle varchar(50),
+    sheduling datetime,
+    delivery datetime,
+    description varchar(500),
+    foreign key (user_id) references User (id) on delete cascade ,
+    foreign key (client_id) references Client (id) on delete cascade ,
+    foreign key (product_id) references Product (id) on delete cascade
 );
- 
-ALTER TABLE Job ADD CONSTRAINT FK_Job_1
-    FOREIGN KEY (fk_User_id)
-    REFERENCES User (id)
-    ON DELETE NO ACTION;
- 
-ALTER TABLE Job ADD CONSTRAINT FK_Job_2
-    FOREIGN KEY (fk_Client_id)
-    REFERENCES Client (id)
-    ON DELETE NO ACTION;
- 
-ALTER TABLE Job ADD CONSTRAINT FK_Job_3
-    FOREIGN KEY (fk_Product_id)
-    REFERENCES Product (id)
-    ON DELETE NO ACTION;
